ca3660e06973ad45bce340d6faa7e5bbaa2b13a0
richasharma3101 <40752689+richasharma3101@users.noreply.github.com>
2018-07-03 20:37:11 +0530

    Multiplication Table of 1 to 10

---
+++ code/Day 0/table.py
@@ -0,0 +1,8 @@
+for i in range(1,11):
+    print(i,end="\t")
+print()
+print("____________________________________________________________________________\n")
+for j in range(1,11):
+    for k in range(1,11):
+        print(j * k,end="\t")
+    print("\n")
