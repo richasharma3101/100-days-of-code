2d3065c3660b44f6bec1fa3db3b2b476536c06bf
richasharma3101 <40752689+richasharma3101@users.noreply.github.com>
2018-07-03 20:34:41 +0530

    Delete table.py

--- code/Day 0/table.py
+++
@@ -1,8 +0,0 @@
-for i in range(1,11):
-    print(i,end="\t")
-print()
-print("____________________________________________________________________________\n")
-for j in range(1,11):
-    for k in range(1,11):
-        print(j * k,end="\t")
-    print("\n")
